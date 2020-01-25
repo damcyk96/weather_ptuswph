8f46a726f73c208039c9d19e1ccf9d667d3ad18c
Damian Obajtek <damian.obajtek@gmail.com>
2020-01-25 23:59:13 +0100

    correct query and response

--- frontend/weather-react/src/App.js
+++ frontend/weather-react/src/App.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 const api = {
   key: "2cedf13fcae3c8e374427e597af727f0",
-  base: "https://api.openweathermap.org/data/2.5"
+  base: "https://api.openweathermap.org/data/2.5/"
 };
 
 function App() {
@@ -12,60 +12,76 @@ function App() {
     if (evt.key === "Enter") {
       fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`)
         .then(res => res.json())
-        .then(result => setWeather(result));
+        .then(result => {
+          setWeather(result);
+          setQuery("");
+          console.log(result);
+        });
     }
   };
-}
 
-const dateBuilder = d => {
-  let months = [
-    "Styczeń",
-    "Luty",
-    "Marzec",
-    "Kwiecień",
-    "Maj",
-    "Czerwiec",
-    "Lipiec",
-    "Sierpień",
-    "Wrzesień",
-    "Październik",
-    "Listopad",
-    "Grudzień"
-  ];
-  let days = [
-    "Poniedziałek",
-    "Wtorek",
-    "Środa",
-    "Czwartek",
-    "Piątek",
-    "Sobota",
-    "Niedziela"
-  ];
+  const dateBuilder = d => {
+    let months = [
+      "Styczeń",
+      "Luty",
+      "Marzec",
+      "Kwiecień",
+      "Maj",
+      "Czerwiec",
+      "Lipiec",
+      "Sierpień",
+      "Wrzesień",
+      "Październik",
+      "Listopad",
+      "Grudzień"
+    ];
+    let days = [
+      "Poniedziałek",
+      "Wtorek",
+      "Środa",
+      "Czwartek",
+      "Piątek",
+      "Sobota",
+      "Niedziela"
+    ];
 
-  let day = days[d.getDay()];
-  let date = d.getDate();
-  let month = months[d.getMonth()];
-  let year = d.getFullYear();
+    let day = days[d.getDay()];
+    let date = d.getDate();
+    let month = months[d.getMonth()];
+    let year = d.getFullYear();
 
-  return `${day} ${date} ${month} ${year}`;
-};
-function App() {
+    return `${day} ${date} ${month} ${year}`;
+  };
+
   return (
     <div className="app">
       <main>
         <div className="search-box">
-          <input type="text" className="search-bar" placeholder="Szukaj..." />
+          <input
+            type="text"
+            className="search-bar"
+            placeholder="Szukaj..."
+            onChange={e => setQuery(e.target.value)}
+            value={query}
+            onKeyPress={search}
+          />
         </div>
-        <div>
-          <div className="location-box">
-            <div className="location">New York City, US</div>
-            <div className="date">{dateBuilder(new Date())}</div>
+        {typeof weather.main != "undefined" ? (
+          <div>
+            <div className="location-box">
+              <div className="location">
+                {weather.name}, {weather.sys.country}
+              </div>
+              <div className="date">{dateBuilder(new Date())}</div>
+            </div>
+            <div className="weather-box">
+              <div className="temp">{Math.round(weather.main.temp)}°C</div>
+              <div className="weather">{weather.weather[0].description}</div>
+            </div>
           </div>
-          <div className="weather-box">
-            <div className="temp">15</div>
-            <div className="weather">Sunny</div>
-          </div>
-        </div>
+        ) : (
+          ""
+        )}
       </main>
     </div>
   );
